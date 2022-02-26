413f4f088040e2e9a3d83107c2a394e1650d9df8
kwt1326 <u1326@hotmail.com>
2022-02-26 23:45:53 +0900

    add: ProductList Page
    ProductLiat Page 추가
    
    필요한 package install
    - mobx
    - mobx-react-lite
    - node-sass # scss 사용을 위해 설치
    - react-query # server-state 관리하기 위해 설치

---
+++ src/ProductList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ProductList.module.scss';
+
+function ProductList(params: any) {
+  return (
+    <div className={styles.container}>
+      TEST
+    </div>
+  )
+}
+
+export default ProductList;
